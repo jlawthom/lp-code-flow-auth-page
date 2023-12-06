d0077db04e3b3d9a4f34cde2274b11b19eec5545
jlawthom <71012108+jlawthom@users.noreply.github.com>
2023-12-06 17:17:14 +0000

    Update code-flow-auth.js

--- code-flow-auth.js
+++ code-flow-auth.js
@@ -142,6 +142,7 @@ var idp = (function() {
 
     // On pageload
     document.addEventListener('DOMContentLoaded', function(){
+        console.log('case01409966-testing');
         console.log('1. Main Frame loaded');
 
         // Get login and logout button ids
